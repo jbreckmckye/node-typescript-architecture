61e46ab067eaa4b0717e09b0a0294cd7fb8f71d3
Jimmy Breck-McKye <jimmy@breck-mckye.com>
2020-05-04 16:25:55 +0000

    Cleanup

--- src/adapters/postgres/client.ts
+++ src/adapters/postgres/client.ts
@@ -1,8 +1,6 @@
 import { Pool, PoolClient } from 'pg'
-import { Context, Operation } from '@lib/context'
 
 export type DbFn <I, O> = (client: PoolClient, input: I) => Promise<O>
-// export type GetClient = () => Promise<PoolClient>
 
 export function createConnectionPool () {
   return new Pool({
@@ -33,25 +31,3 @@ export function withClient <I, O> (client: PoolClient, fn: DbFn<I, O>) {
     return fn(client, input)
   }
 }
-
-
-// export function wrapOperation <I, O, Op = Operation<I, O>> (clientP: Promise<PoolClient>, op: Op): Op {
-//   return async function (ctx: Context, input: I): Promise<O> {
-//     const client = await clientP
-//
-//     try {
-//       await client.query('BEGIN')
-//       const result: O = await op(ctx, input)
-//       await client.query('COMMIT')
-//       return result
-//
-//     } catch (e) {
-//       await client.query('ROLLBACK')
-//       throw e
-//
-//     } finally {
-//       client.release()
-//     }
-//   }
-// }
-//
--- src/adapters/postgres/index.ts
+++ src/adapters/postgres/index.ts
@@ -34,4 +34,3 @@ export function $adapter (): ContextAdapter {
     }
   }
 }
-
